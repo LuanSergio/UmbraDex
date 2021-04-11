24b43297d91bd613356a99185a07e25459fe2b62
Luan Sergio <luansergiomattos@gmail.com>
2021-04-11 17:50:23 -0300

    perf: changed the way pokemon request were being made

--- src/components/layouts/HomeLayout/index.tsx
+++ src/components/layouts/HomeLayout/index.tsx
@@ -9,43 +9,49 @@ const HomeLayout = (): JSX.Element => {
     return response.data.pokemon_entries.length;
   }
 
-  async function getPokemonDataList() {
+  async function fetchPokemonData() {
     const quantity: number = await getPokemonQuantity();
-    const pokemonDataList = [];
+    const pokemonUrlList = [];
 
     for (let i = 1; i <= quantity; i++) {
       try {
         const url = `https://pokeapi.co/api/v2/pokemon/${i}/`;
-        const response = await axios.get(url);
-        const pokemonData = {
-          name: response.data.name,
-          image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${response.data.id}.png`,
-          types: response.data.types.map(item => item.type.name),
-          id: response.data.id,
-        };
-        pokemonDataList.push(pokemonData);
+        pokemonUrlList.push(url);
       } catch (error) {
         console.log(`Error occured on main endpoint: ${error.message}`);
         const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-        const response = await axios.get(url);
-        const pokemonData = {
-          name: response.data.name,
-          image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${response.data.id}.png`,
-          types: response.data.types.map(item => item.type.name),
-          id: response.data.id,
-        };
-        pokemonDataList.push(pokemonData);
+        pokemonUrlList.push(url);
       }
     }
-    console.clear();
-    return Array.from(pokemonDataList);
+    const promises = pokemonUrlList.map(url => axios.get(url));
+    const response = await axios.all(promises);
+
+    return response;
+  }
+
+  async function getPokemonInformation() {
+    const responses = await fetchPokemonData();
+    const pokemonInformation = [];
+
+    responses.forEach(pokemon => {
+      const pokemonData = {
+        name: pokemon.data.name,
+        image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.data.id}.png`,
+        types: pokemon.data.types.map(item => item.type.name),
+        id: pokemon.data.id,
+      };
+
+      pokemonInformation.push(pokemonData);
+    });
+
+    return pokemonInformation;
   }
 
   const [pokemons, setPokemons] = useState([]);
 
   useEffect(() => {
     async function setPokemonData() {
-      setPokemons(await getPokemonDataList());
+      setPokemons(await getPokemonInformation());
     }
     setPokemonData();
   }, []);
