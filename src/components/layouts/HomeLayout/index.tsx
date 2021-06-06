a66e1d47129230a2d89721f2d1bb5c7372188ac8
Luan <luansergiomattos@gmail.com>
2021-06-06 19:04:06 -0300

    fix: bug with nanoid that caused flickering when loading more cards

--- src/components/layouts/HomeLayout/index.tsx
+++ src/components/layouts/HomeLayout/index.tsx
@@ -1,7 +1,6 @@
 import { useRef } from 'react';
 import PokemonCard from '@components/molecules/PokemonCard';
 import Header from '@components/molecules/Header';
-import { nanoid } from 'nanoid';
 import styles from './styles.module.scss';
 import useController from './index.controller';
 
@@ -17,7 +16,7 @@ const HomeLayout = (): JSX.Element => {
           {pokemon.length > 1 && (
             <>
               {pokemon.map(element => (
-                <li key={nanoid()}>
+                <li key={element.id}>
                   <PokemonCard
                     id={element.id}
                     name={element.name}
