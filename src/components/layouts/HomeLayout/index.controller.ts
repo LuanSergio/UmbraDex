59538da5387229933775817589cfc316f7dcb579
Luan <luansergiomattos@gmail.com>
2021-06-05 23:14:07 -0300

    refactor: changed variables and function names on HomeLayout controller

--- src/components/layouts/HomeLayout/index.controller.ts
+++ src/components/layouts/HomeLayout/index.controller.ts
@@ -6,18 +6,56 @@ interface IController {
 }
 
 const useController = (loader: MutableRefObject<HTMLElement>): IController => {
-  const [pokemonCounter, setPokemonCounter] = useState(0);
-  const [pokemonList, setPokemonList] = useState<IPokemonData[]>([]);
-  const [pokemon, setPokemon] = useState<IPokemonData[]>([]);
+  const [loadedPokemonCounter, setLoadedPokemonCounter] = useState(0);
+  const [pokemonDataList, setPokemonDataList] = useState<IPokemonData[]>([]);
+  const [loadedPokemon, setLoadedPokemon] = useState<IPokemonData[]>([]);
   const [pageYOffset, setPageYOffset] = useState(0);
 
   const handleObserver = (entities: IntersectionObserverEntry[]) => {
     const target = entities[0];
     if (target.isIntersecting) {
       setPageYOffset(window.pageYOffset);
-      setPokemonCounter(counter => counter + 100);
+      setLoadedPokemonCounter(counter => counter + 100);
     }
   };
+
+  async function getAvailablePokemonQuantity() {
+    const response = await axios.get('https://pokeapi.co/api/v2/pokedex/1/');
+    return response.data.pokemon_entries.length;
+  }
+
+  async function fetchPokemonData() {
+    const quantity: number = await getAvailablePokemonQuantity();
+    const pokemonUrlList = [];
+
+    for (let i = 1; i <= quantity; i++) {
+      const url = `https://pokeapi.co/api/v2/pokemon/${i}/`;
+      pokemonUrlList.push(url);
+    }
+
+    const promises = pokemonUrlList.map(url => axios.get(url));
+    const responses = await axios.all(promises);
+
+    return responses;
+  }
+
+  async function getPokemonDataList() {
+    const responses = await fetchPokemonData();
+    const pokemonDataArray = [];
+
+    responses.forEach(response => {
+      const pokemonData: IPokemonData = {
+        id: response.data.id,
+        name: response.data.name,
+        types: response.data.types.map(item => item.type.name),
+        image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${response.data.id}.png`,
+      };
+
+      pokemonDataArray.push(pokemonData);
+    });
+
+    return pokemonDataArray;
+  }
 
   useEffect(() => {
     const options = {
@@ -29,70 +67,32 @@ const useController = (loader: MutableRefObject<HTMLElement>): IController => {
     if (loader?.current) {
       observer.observe(loader.current);
     }
-  }, []);
+  }, [loader]);
 
   useEffect(() => {
-    window.scroll({ top: pageYOffset });
-  }, [pokemon]);
-
-  useEffect(() => {
-    const count = pokemon.length;
-    setPokemon(previousState => [
+    const count = loadedPokemon.length;
+    setLoadedPokemon(previousState => [
       ...previousState,
-      ...pokemonList.slice(count, count + 40),
+      ...pokemonDataList.slice(count, count + 40),
     ]);
-  }, [pokemonCounter]);
-
-  async function getPokemonQuantity() {
-    const response = await axios.get('https://pokeapi.co/api/v2/pokedex/1/');
-    return response.data.pokemon_entries.length;
-  }
-
-  async function fetchPokemonData() {
-    const quantity: number = await getPokemonQuantity();
-    const pokemonUrlList = [];
-
-    for (let i = 1; i <= quantity; i++) {
-      const url = `https://pokeapi.co/api/v2/pokemon/${i}/`;
-      pokemonUrlList.push(url);
-    }
-
-    const promises = pokemonUrlList.map(url => axios.get(url));
-    const response = await axios.all(promises);
-
-    return response;
-  }
-
-  async function getPokemonInformation() {
-    const responses = await fetchPokemonData();
-    const pokemonInformation = [];
-
-    responses.forEach(response => {
-      const pokemonData: IPokemonData = {
-        id: response.data.id,
-        name: response.data.name,
-        types: response.data.types.map(item => item.type.name),
-        image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${response.data.id}.png`,
-      };
-
-      pokemonInformation.push(pokemonData);
-    });
-
-    return pokemonInformation;
-  }
+  }, [loadedPokemonCounter, pokemonDataList]);
 
   useEffect(() => {
-    setPokemon([...pokemonList.slice(0, 24)]);
-  }, [pokemonList]);
+    setLoadedPokemon([...pokemonDataList.slice(0, 24)]);
+  }, [pokemonDataList]);
 
   useEffect(() => {
     async function setPokemonData() {
-      setPokemonList(await getPokemonInformation());
+      setPokemonDataList(await getPokemonDataList());
     }
     setPokemonData();
   }, []);
 
-  return { pokemon };
+  useEffect(() => {
+    window.scroll({ top: pageYOffset });
+  }, [loadedPokemon, pageYOffset]);
+
+  return { pokemon: loadedPokemon };
 };
 
 export default useController;
