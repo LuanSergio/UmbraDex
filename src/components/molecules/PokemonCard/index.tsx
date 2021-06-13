4c5623acce16b3c539799b5257ccd98757cee941
Luan <luansergiomattos@gmail.com>
2021-06-13 18:36:43 -0300

    feat: add link to pokemon cards

--- src/components/molecules/PokemonCard/index.tsx
+++ src/components/molecules/PokemonCard/index.tsx
@@ -1,39 +1,45 @@
+import Link from 'next/link';
 import styles from './styles.module.scss';
 import PokeballIcon from '../../../assets/images/icons/pokeball.svg';
 
 const PokemonCard = ({ id, name, types, image }: IPokemonData): JSX.Element => {
   return (
-    <div className={styles.card}>
-      {id <= 10 ? (
-        <span className={styles.number}># 0{id}</span>
-      ) : (
-        <span className={styles.number}># {id}</span>
-      )}
+    <Link href={`/pokemon/${id}`}>
+      <div className={styles.card}>
+        {id <= 10 ? (
+          <span className={styles.number}># 0{id}</span>
+        ) : (
+          <span className={styles.number}># {id}</span>
+        )}
 
-      <div className={styles.pokemonContainer}>
-        <span className={styles.pokeball}>
-          <PokeballIcon />
-        </span>
-        <img
-          loading="lazy"
-          className={styles.pokemon}
-          src={image}
-          alt={name}
-          title={name}
-        />
-        <span className={`${styles.name} ${styles[types[0]]}`}>{name}</span>
-      </div>
-
-      <div className={styles.typeContainer}>
-        {types.map((type, index) => (
-          <span key={type} className={`${styles.type} ${styles[types[index]]}`}>
-            {type}
+        <div className={styles.pokemonContainer}>
+          <span className={styles.pokeball}>
+            <PokeballIcon />
           </span>
-        ))}
-      </div>
+          <img
+            loading="lazy"
+            className={styles.pokemon}
+            src={image}
+            alt={name}
+            title={name}
+          />
+          <span className={`${styles.name} ${styles[types[0]]}`}>{name}</span>
+        </div>
 
-      <div className={`${styles.background} ${styles[types[0]]}`} />
-    </div>
+        <div className={styles.typeContainer}>
+          {types.map((type, index) => (
+            <span
+              key={type}
+              className={`${styles.type} ${styles[types[index]]}`}
+            >
+              {type}
+            </span>
+          ))}
+        </div>
+
+        <div className={`${styles.background} ${styles[types[0]]}`} />
+      </div>
+    </Link>
   );
 };
 
