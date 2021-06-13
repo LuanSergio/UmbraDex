9a9b17cc635bfcf50eed888e0dc000fe4889bf41
Luan <luansergiomattos@gmail.com>
2021-06-13 19:07:29 -0300

    feat: add basic pokemon information to pokemon page

--- src/pages/pokemon/[name].tsx
+++ src/pages/pokemon/[name].tsx
@@ -4,17 +4,36 @@ import Header from '@components/molecules/Header';
 
 interface IPokemonDetails {
   name: string;
+  id: number;
+  types: string[];
+  image: string;
+}
+
+interface ISpeciesDetail {
+  description: string;
+  originalName: string;
 }
 
 interface IPokemonDetailsProps {
   pokemon: IPokemonDetails;
+  speciesDetail: ISpeciesDetail;
 }
 
-const Pokemon = ({ pokemon }: IPokemonDetailsProps): JSX.Element => {
+const Pokemon = ({
+  pokemon,
+  speciesDetail,
+}: IPokemonDetailsProps): JSX.Element => {
   return (
     <>
       <Header />
-      <h1>{pokemon?.name}</h1>
+      <main>
+        <img src={pokemon.image} alt={pokemon.name} />
+        <span>{pokemon.id}</span>
+        <span>{speciesDetail.originalName}</span>
+
+        <h1>{pokemon?.name}</h1>
+        <p>{speciesDetail.description}</p>
+      </main>
     </>
   );
 };
@@ -29,15 +48,26 @@ export const getStaticPaths: GetStaticPaths = async () => {
 export const getStaticProps: GetStaticProps = async context => {
   const { name } = context.params;
 
-  const { data } = await api.get(`pokemon/${name}/`);
+  const { data: pokemonData } = await api.get(`pokemon/${name}/`);
+
+  const { data: speciesData } = await api.get(`pokemon-species/${name}/`);
 
   const pokemon = {
-    name: data.name,
+    name: pokemonData.name,
+    id: pokemonData.id,
+    types: pokemonData.types.map(item => item.type.name),
+    image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonData.id}.png`,
+  };
+
+  const speciesDetail = {
+    description: speciesData.flavor_text_entries[0].flavor_text,
+    originalName: speciesData.names[0].name,
   };
 
   return {
     props: {
       pokemon,
+      speciesDetail,
     },
     revalidate: 60 * 60 * 24, // 24 hours
   };
