d09a2ad3b987c3ff5adac10aa724bb0f3c14d9c1
Luan <luansergiomattos@gmail.com>
2021-06-14 19:08:06 -0300

    chore: add sass default path

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 // next.config.js
 const withImages = require('next-images');
+
 module.exports = withImages({
   esModule: true,
+  sassOptions: {
+    includePaths: "./src/assets/styles",
+  },
 });
