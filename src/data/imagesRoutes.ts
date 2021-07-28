025aeb2962bdf896d17eca2931def4892b0d644d
Luan <luansergiomattos@gmail.com>
2021-07-28 19:44:23 -0300

    feat: add imagesRoutes data file

---
+++ src/data/imagesRoutes.ts
@@ -0,0 +1,7 @@
+const pokemonArtworkImages = {
+  main: 'https://umbradex.vercel.app/artworks',
+  fallback:
+    'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork',
+};
+
+export default pokemonArtworkImages;
