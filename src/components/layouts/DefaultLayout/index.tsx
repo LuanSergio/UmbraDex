ed9b44eb6f433f228238883d8c801edff733cfc9
Luan <luan.sergio@novahaus.com.br>
2021-08-28 20:58:56 -0300

    refactor: remove classes prop from default layout

--- src/components/layouts/DefaultLayout/index.tsx
+++ src/components/layouts/DefaultLayout/index.tsx
@@ -3,23 +3,15 @@ import { ReactNode } from 'react';
 
 interface DefaultLayoutProps {
   children: ReactNode;
-  classes?: string;
 }
 
-const HomeLayout = ({
-  children,
-  classes = '',
-}: DefaultLayoutProps): JSX.Element => {
+const HomeLayout = ({ children }: DefaultLayoutProps): JSX.Element => {
   return (
     <>
       <Header />
-      <main className={`h-container ${classes}`}>{children}</main>
+      <main className="h-container">{children}</main>
     </>
   );
-};
-
-HomeLayout.defaultProps = {
-  classes: '',
 };
 
 export default HomeLayout;
