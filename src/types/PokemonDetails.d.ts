c56c901c539fb66dbb7fb20ba9a37044577c3165
Luan <luan.sergio@novahaus.com.br>
2021-09-04 13:44:01 -0300

    refactor: move pokemon form interface to another file

--- src/types/PokemonDetails.d.ts
+++ src/types/PokemonDetails.d.ts
@@ -10,25 +10,7 @@ declare interface PokemonEvolution {
   order: number;
 }
 
-declare interface PokemonStats {
-  name: string;
-  value: number;
-}
-
-declare interface PokemonForm {
-  id: number;
-  name: string;
-  isDefault: boolean;
-  formsDetails;
-  types: string[];
-  formName: string;
-  formOrder: string;
-  stats: PokemonStats;
-  image: string;
-}
-
 declare interface IPokemonDetails {
-  forms: PokemonForm;
   japaneseName: string;
   descriptions: PokemonDescription[];
   evolutionChain: PokemonEvolution[];
---
+++ src/types/PokemonForm.d.ts
@@ -0,0 +1,16 @@
+declare interface PokemonStats {
+  name: string;
+  value: number;
+}
+
+declare interface PokemonForm {
+  id: number;
+  name: string;
+  isDefault: boolean;
+  formsDetails;
+  types: string[];
+  formName: string;
+  formOrder: string;
+  stats: PokemonStats;
+  image: string;
+}
