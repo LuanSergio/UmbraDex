aa37ef5b787b78b24155c813ed666aad544ef611
Luan <luan.sergio@novahaus.com.br>
2021-09-04 15:54:36 -0300

    chore: remove next-libary

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 // next.config.js
-const withImages = require('next-images');
 
-module.exports = withImages({
+module.exports = {
   esModule: true,
   sassOptions: {
     includePaths: "./src/assets/styles",
   },
-});
+};
