42edd08a2e782ada07e461599904c4ae7b95144e
Luan <luan.sergio@novahaus.com.br>
2022-04-09 21:34:09 -0300

    feat: add carousel to evolution chain

--- src/components/molecules/EvolutionChain/index.tsx
+++ src/components/molecules/EvolutionChain/index.tsx
@@ -1,6 +1,10 @@
+import { useState } from 'react';
 import Link from 'next/link';
+
 import getPokemonImageUrl from '@utils/getPokemonImageUrl';
 import transformFirstLetterToUppercase from '@utils/transformFirstLetterToUppercase';
+import Carousel from '@components/molecules/Carousel';
+import CarouselItem from '@components/atoms/CarouselItem';
 import styles from './styles.module.scss';
 
 interface IEvolutionChainProps {
@@ -12,37 +16,100 @@ const EvolutionChain = ({
   evolutionChain,
   currentId,
 }: IEvolutionChainProps): JSX.Element => {
+  const [evolutionIndex, setEvolutionIndex] = useState(0);
+
+  function handleEvolutionIndexChange(index: number): void {
+    setEvolutionIndex(index);
+  }
+
   return (
-    <div className={styles.evolutionChain}>
-      {evolutionChain.map(evolution => {
-        return (
-          <Link href={`/pokemon/${evolution.id}`} key={evolution.id} passHref>
-            <a
-              className={` ${styles.evolutionContainer} ${
-                currentId === evolution.id
-                  ? styles.evolutionContainerActive
-                  : ''
-              }`}
-            >
-              <div
-                title={transformFirstLetterToUppercase(evolution.name)}
-                role="img"
-                id={evolution.name}
-                aria-labelledby={evolution.name}
-                className={styles.evolution}
-                style={{
-                  WebkitMaskImage: `url(${getPokemonImageUrl(evolution.id)})`,
-                  maskImage: `url(${getPokemonImageUrl(evolution.id)})`,
-                }}
-              />
-              <span id={evolution.name} className={styles.evolutionName}>
-                {evolution.name}
-              </span>
-            </a>
-          </Link>
-        );
-      })}
-    </div>
+    <>
+      {evolutionChain.length > 3 ? (
+        <div className={styles.evolutionChainCarouselContainer}>
+          <Carousel
+            currentIndex={evolutionIndex}
+            updateCurrentIndex={handleEvolutionIndexChange}
+            tagName="ul"
+            itemWidth={120}
+            gap={20}
+            maxItems={3}
+          >
+            {evolutionChain.map(evolution => {
+              return (
+                <CarouselItem key={evolution.id} tagName="li">
+                  <Link href={`/pokemon/${evolution.id}`} passHref>
+                    <a
+                      className={` ${styles.evolutionContainer} ${
+                        currentId === evolution.id
+                          ? styles.evolutionContainerActive
+                          : ''
+                      }`}
+                    >
+                      <div
+                        title={transformFirstLetterToUppercase(evolution.name)}
+                        role="img"
+                        id={evolution.name}
+                        aria-labelledby={evolution.name}
+                        className={styles.evolution}
+                        style={{
+                          WebkitMaskImage: `url(${getPokemonImageUrl(
+                            evolution.id,
+                          )})`,
+                          maskImage: `url(${getPokemonImageUrl(evolution.id)})`,
+                        }}
+                      />
+                      <span
+                        id={evolution.name}
+                        className={styles.evolutionName}
+                      >
+                        {evolution.name}
+                      </span>
+                    </a>
+                  </Link>
+                </CarouselItem>
+              );
+            })}
+          </Carousel>
+        </div>
+      ) : (
+        <div className={styles.evolutionChain}>
+          {evolutionChain.map(evolution => {
+            return (
+              <Link
+                href={`/pokemon/${evolution.id}`}
+                key={evolution.id}
+                passHref
+              >
+                <a
+                  className={` ${styles.evolutionContainer} ${
+                    currentId === evolution.id
+                      ? styles.evolutionContainerActive
+                      : ''
+                  }`}
+                >
+                  <div
+                    title={transformFirstLetterToUppercase(evolution.name)}
+                    role="img"
+                    id={evolution.name}
+                    aria-labelledby={evolution.name}
+                    className={styles.evolution}
+                    style={{
+                      WebkitMaskImage: `url(${getPokemonImageUrl(
+                        evolution.id,
+                      )})`,
+                      maskImage: `url(${getPokemonImageUrl(evolution.id)})`,
+                    }}
+                  />
+                  <span id={evolution.name} className={styles.evolutionName}>
+                    {evolution.name}
+                  </span>
+                </a>
+              </Link>
+            );
+          })}
+        </div>
+      )}
+    </>
   );
 };
 
