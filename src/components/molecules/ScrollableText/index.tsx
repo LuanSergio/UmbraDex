b5c4e6a5f40ee27240c46b7bd05ac0fb24383de3
Luan <luan.sergio@novahaus.com.br>
2022-04-22 22:56:24 -0300

    feat: add ScrollableText component

---
+++ src/components/molecules/ScrollableText/index.tsx
@@ -0,0 +1,45 @@
+import { ReactNode, useEffect, useRef, useState, Children } from 'react';
+
+import getElementHeight from '@utils/getElementHeight';
+
+import styles from './styles.module.scss';
+
+interface IScrollableTextProps {
+  children: ReactNode;
+  maxHeight: number;
+  shouldChange?: unknown;
+}
+
+const ScrollableText = ({
+  children,
+  shouldChange,
+  maxHeight,
+}: IScrollableTextProps): JSX.Element => {
+  const [height, setHeight] = useState(0);
+  const textRef = useRef<HTMLParagraphElement>(null);
+
+  useEffect(() => {
+    setHeight(getElementHeight(textRef.current));
+  }, [shouldChange]);
+
+  return (
+    <>
+      <div
+        className={`${styles.scrollableText} ${
+          height > maxHeight ? styles.scrollableTextOverflowContained : ''
+        }`}
+      >
+        <div
+          ref={textRef}
+          className={
+            height > maxHeight ? styles.scrollableTextContentContained : ''
+          }
+        >
+          {children}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ScrollableText;
