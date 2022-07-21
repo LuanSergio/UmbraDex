c1b1be65f93666ceb10a9f794b2ab6f894247819
Luan <luan.sergio@novahaus.com.br>
2022-07-20 21:27:05 -0300

    feat: add animation to webdoor

--- src/components/molecules/WebDoor/index.tsx
+++ src/components/molecules/WebDoor/index.tsx
@@ -1,6 +1,9 @@
+import { motion } from 'framer-motion';
+
 import Gengar from '@public/webdoor/gengar/gengar.svg';
 import Crobat from '@public/webdoor/gengar/crobat.svg';
 import Zubat from '@public/webdoor/gengar/zubat.svg';
+
 import styles from './styles.module.scss';
 
 const WebDoor = (): JSX.Element => {
@@ -14,18 +17,55 @@ const WebDoor = (): JSX.Element => {
   return (
     <section className={styles.webDoor}>
       <div className={`${styles.container} h-container`}>
-        <div className={styles.titleHolder}>
-          <h1 className={styles.title}>{title}</h1>
+        <div className={styles.titleContainer}>
+          <div className={styles.titleHolder}>
+            <h1 className={styles.title}>{title}</h1>
 
-          <span aria-hidden="true" className={styles.titleShadow}>
-            {title}
-          </span>
+            <span aria-hidden="true" className={styles.titleShadow}>
+              {title}
+            </span>
+          </div>
         </div>
-        <Gengar className={styles.gengar} />
-        <Crobat className={styles.crobat} />
-        <Zubat className={styles.zubat1} />
-        <Zubat className={styles.zubat2} />
-        <Zubat className={styles.zubat3} />
+
+        <motion.div
+          transition={{ delay: 0.4, type: 'tween', duration: 1 }}
+          initial={{ x: '-300%', y: '400vh' }}
+          animate={{ y: 0, x: 0 }}
+        >
+          <Zubat className={styles.zubat1} />
+        </motion.div>
+
+        <motion.div
+          transition={{ delay: 0.4, type: 'tween', duration: 1 }}
+          initial={{ x: '-300%', y: '400vh' }}
+          animate={{ y: 0, x: 0 }}
+        >
+          <Zubat className={styles.zubat2} />
+        </motion.div>
+
+        <motion.div
+          transition={{ delay: 0.4, type: 'tween', duration: 1 }}
+          initial={{ x: '-300%', y: '400vh' }}
+          animate={{ x: 0, y: '0' }}
+        >
+          <Zubat className={styles.zubat3} />
+        </motion.div>
+
+        <motion.div
+          className={styles.crobatHolder}
+          transition={{ delay: 0.75, type: 'tween', duration: 0.75 }}
+          initial={{ x: '-300%', y: '400vh' }}
+          animate={{ y: 0, x: 0 }}
+        >
+          <Crobat className={styles.crobat} />
+        </motion.div>
+        <motion.div
+          transition={{ delay: 1.75, type: 'tween', duration: 0.75 }}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+        >
+          <Gengar className={styles.gengar} />
+        </motion.div>
       </div>
     </section>
   );
