840869b21805901a96c1f266a3efef4f6b545a27
Luan <luan.sergio@novahaus.com.br>
2022-07-27 20:26:41 -0300

    feat: get the webdoor height in a dynamic way

--- src/components/layouts/DefaultLayout/index.tsx
+++ src/components/layouts/DefaultLayout/index.tsx
@@ -1,13 +1,24 @@
-import { ReactNode } from 'react';
+import { ReactNode, useEffect } from 'react';
 
 import Header from '@components/molecules/Header';
 import WebDoor from '@components/molecules/WebDoor';
+import useWindowSize from '@hooks/useWindowSize';
 
 interface DefaultLayoutProps {
   children: ReactNode;
 }
 
 const DefaultLayout = ({ children }: DefaultLayoutProps): JSX.Element => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [_, windowHeight] = useWindowSize();
+
+  useEffect(() => {
+    document.documentElement.style.setProperty(
+      '--webdoor-height',
+      `${document.documentElement.clientHeight.toString()}px`,
+    );
+  }, [windowHeight]);
+
   return (
     <>
       <Header />
