52a3b71cbcea339f7d75b9bb50637a86b9edebf6
Luan <luan.sergio@novahaus.com.br>
2022-10-22 20:20:23 -0300

    feat: add text field component

---
+++ src/components/atoms/TextField/index.tsx
@@ -0,0 +1,50 @@
+import { InputHTMLAttributes, Ref } from 'react';
+
+import styles from './styles.module.scss';
+
+interface ITextFieldProps {
+  label: string;
+  helperText?: string;
+  adornment?: JSX.Element;
+  size?: 'small' | 'normal';
+  onAdornmentClick?: () => void;
+  value: string;
+  theme?: 'primary' | 'secondary';
+  inputProps?: InputHTMLAttributes<HTMLInputElement> & {
+    ref?: Ref<HTMLInputElement>;
+  };
+}
+
+const TextField = ({
+  label,
+  helperText,
+  value,
+  theme = 'primary',
+  inputProps,
+  adornment,
+  size = 'normal',
+}: ITextFieldProps): JSX.Element => (
+  <div className={styles.textField}>
+    <div className={`${styles.container} ${styles[theme]} ${styles[size]}`}>
+      <input
+        {...inputProps}
+        value={value}
+        className={`${styles.input} ${
+          adornment ? styles.inputWithAdornment : ''
+        }`}
+      />
+      <label
+        className={`${styles.label} ${
+          value.length > 0 ? styles.labelTransformed : ''
+        }`}
+        htmlFor={inputProps?.name}
+      >
+        {label}
+      </label>
+      <div className={styles.adornment}>{adornment}</div>
+    </div>
+    {helperText && <span className={styles.textHelper}>{helperText}</span>}
+  </div>
+);
+
+export default TextField;
