c37541782a84e96eb8baee002210ebf18a1326d0
Luan <luan.sergio@novahaus.com.br>
2022-11-13 03:58:33 -0300

    feat: add menu component

---
+++ src/components/molecules/Menu/index.tsx
@@ -0,0 +1,94 @@
+import { useState } from 'react';
+
+import SortIcon from '@public/icons/sort.svg';
+import RandomIcon from '@public/icons/random.svg';
+import MoonIcon from '@public/icons/moon.svg';
+import FilterIcon from '@public/icons/filter.svg';
+import DocumentIcon from '@public/icons/document.svg';
+
+import SwitchToggle from '@components/atoms/SwitchToggle';
+import Modal from '@components/atoms/Modal';
+
+import styles from './styles.module.scss';
+
+interface IMenuProps {
+  isOpen: boolean;
+}
+
+const Menu = ({ isOpen }: IMenuProps): JSX.Element => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  function handleDarkModeToggle() {
+    setIsDarkMode(previousValue => !previousValue);
+  }
+
+  return (
+    <>
+      {isOpen && (
+        <div className={styles.menu}>
+          <ul className={styles.optionsList}>
+            <li className={styles.optionsListItem}>
+              <button
+                type="button"
+                className={`${styles.button} ${styles.buttonToggle}`}
+                onClick={handleDarkModeToggle}
+              >
+                <span className={styles.toggleContainer}>
+                  <span className={styles.icon}>
+                    <MoonIcon />
+                  </span>
+                  Dark mode
+                </span>
+                <SwitchToggle isChecked={isDarkMode} name="dark-mode" />
+              </button>
+            </li>
+          </ul>
+
+          <ul className={styles.optionsList}>
+            <li className={styles.optionsListItem}>
+              <button type="button" className={styles.button}>
+                <span className={styles.icon}>
+                  <RandomIcon />
+                </span>
+                Random
+              </button>
+            </li>
+
+            <li className={styles.optionsListItem}>
+              <button type="button" className={styles.button}>
+                <span className={styles.icon}>
+                  <FilterIcon />
+                </span>
+                Filters
+              </button>
+            </li>
+
+            <li className={styles.optionsListItem}>
+              <button type="button" className={styles.button}>
+                <span className={styles.icon}>
+                  <SortIcon />
+                </span>
+                Sort
+              </button>
+            </li>
+          </ul>
+
+          <ul className={styles.optionsList}>
+            <li className={styles.optionsListItem}>
+              <Modal>
+                <button type="button" className={styles.button}>
+                  <span className={styles.icon}>
+                    <DocumentIcon />
+                  </span>
+                  Disclaimer
+                </button>
+              </Modal>
+            </li>
+          </ul>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Menu;
