bf333d22de5b3d32d892e4fa32d422204e270593
Luan <luan.sergio@novahaus.com.br>
2022-11-13 04:00:21 -0300

    chore: change header structure

--- src/components/molecules/Header/index.tsx
+++ src/components/molecules/Header/index.tsx
@@ -5,23 +5,29 @@ import Logo from '@public/icons/logo.svg';
 import SearchIcon from '@public/icons/search.svg';
 import Github from '@public/icons/github.svg';
 import SearchInput from '@components/molecules/SearchInput';
+import Menu from '@components/molecules/Menu';
 import styles from './styles.module.scss';
 
 const Header = (): JSX.Element => {
-  const headerRef = useRef(null);
+  const headerBackgroundRef = useRef(null);
   const [isSearchOpen, setIsSearchOpen] = useState(false);
+  const [isOptionsOpen, setIsOptionsOpen] = useState(false);
 
   const hasScrolled = () => {
     const scrollTop = window.scrollY;
     if (scrollTop > 0) {
-      headerRef.current?.classList.add(styles.scrolled);
+      headerBackgroundRef.current?.classList.add(styles.scrolled);
     } else {
-      headerRef.current?.classList.remove(styles.scrolled);
+      headerBackgroundRef.current?.classList.remove(styles.scrolled);
     }
   };
 
   function handleOpenSearchClick() {
     setIsSearchOpen(previousState => !previousState);
+  }
+
+  function handleOpenOptionsClick() {
+    setIsOptionsOpen(previousState => !previousState);
   }
 
   useEffect(() => {
@@ -32,54 +38,63 @@ const Header = (): JSX.Element => {
   });
 
   return (
-    <header className={styles.header} ref={headerRef}>
-      <div className={`${styles.headerContainer} h-container`}>
-        <div className={`${isSearchOpen ? `${styles.hiddenMobileItem}` : ''}`}>
-          <Link href="/">
-            <a className={styles.logo} aria-label="UmbraDex">
-              <Logo />
+    <header>
+      <div className={styles.header}>
+        <div className={`${styles.headerContainer} h-container`}>
+          <div
+            className={`${isSearchOpen ? `${styles.hiddenMobileItem}` : ''}`}
+          >
+            <Link href="/">
+              <a className={styles.logo} aria-label="UmbraDex">
+                <Logo />
+              </a>
+            </Link>
+          </div>
+
+          <div className={styles.search}>
+            <SearchInput isOpen={isSearchOpen} />
+          </div>
+
+          <div className={styles.rightContainer}>
+            <a
+              href="https://github.com/LuanSergio/UmbraDex"
+              target="_blank"
+              aria-label="GitHub"
+              title="GitHub"
+              className={`${styles.github} ${
+                isSearchOpen ? `${styles.hiddenMobileItem}` : ''
+              }`}
+              rel="noreferrer"
+            >
+              <Github />
             </a>
-          </Link>
-        </div>
 
-        <div className={styles.search}>
-          <SearchInput isOpen={isSearchOpen} />
-        </div>
+            <button
+              aria-label="Search"
+              title="Search"
+              className={styles.searchIcon}
+              type="button"
+              onClick={handleOpenSearchClick}
+            >
+              <SearchIcon />
+            </button>
 
-        <div className={styles.rightContainer}>
-          <a
-            href="https://github.com/LuanSergio/UmbraDex"
-            target="_blank"
-            aria-label="GitHub"
-            title="GitHub"
-            className={`${styles.github} ${
-              isSearchOpen ? `${styles.hiddenMobileItem}` : ''
-            }`}
-            rel="noreferrer"
-          >
-            <Github />
-          </a>
-
-          <button
-            aria-label="Search"
-            title="Search"
-            className={styles.searchIcon}
-            type="button"
-            onClick={handleOpenSearchClick}
-          >
-            <SearchIcon />
-          </button>
-
-          <button
-            type="button"
-            className={`${styles.options} ${
-              isSearchOpen ? `${styles.hiddenMobileItem}` : ''
-            }`}
-            aria-label="options"
-            title="options"
-          />
+            <div className={styles.optionsMenu}>
+              <button
+                type="button"
+                className={`${styles.options} ${
+                  isSearchOpen ? `${styles.hiddenMobileItem}` : ''
+                }`}
+                aria-label="options"
+                title="options"
+                onClick={handleOpenOptionsClick}
+              />
+              <Menu isOpen={isOptionsOpen} />
+            </div>
+          </div>
         </div>
       </div>
+      <div className={styles.headerBackground} ref={headerBackgroundRef} />
     </header>
   );
 };
