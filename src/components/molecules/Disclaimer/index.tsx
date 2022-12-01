22c8c6876c2c7067b275dc81bfaf77781ab16a64
Luan <luan.sergio@novahaus.com.br>
2022-11-30 22:13:25 -0300

    feat: add disclaimer component

---
+++ src/components/molecules/Disclaimer/index.tsx
@@ -0,0 +1,20 @@
+import styles from './styles.module.scss';
+
+const Disclaimer = (): JSX.Element => {
+  return (
+    <p className={styles.disclaimerText}>
+      This website was made as a study project, with no no financial purpose.{' '}
+      <br />
+      <br />
+      All image contents within are Copyright The Pokémon Company. Pokémon ©
+      2002-2023 Pokémon. © 1995-2023 Nintendo/Creatures Inc./GAME FREAK inc. TM,
+      ® and Pokémon character names are trademarks of Nintendo.
+      <br />
+      <br />
+      No copyright or trademark infringement is intended in using Pokémon
+      content on this website.
+    </p>
+  );
+};
+
+export default Disclaimer;
