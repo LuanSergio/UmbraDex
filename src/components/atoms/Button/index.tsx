3e684caead03c882dd6c3bfabe55ba976d14baa3
Luan <luan.sergio@novahaus.com.br>
2022-12-18 22:59:26 -0300

    feat: add secondary button theme and button props option

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -1,12 +1,24 @@
+import { ButtonHTMLAttributes } from 'react';
+
 import styles from './styles.module.scss';
 
 interface IButtonProps {
   children: string;
+  theme?: 'primary' | 'secondary';
+  buttonProps?: ButtonHTMLAttributes<HTMLButtonElement>;
 }
 
-const Button = ({ children }: IButtonProps): JSX.Element => {
+const Button = ({
+  children,
+  buttonProps,
+  theme = 'primary',
+}: IButtonProps): JSX.Element => {
   return (
-    <button type="button" className={styles.button}>
+    <button
+      type="button"
+      className={`${styles.button} ${styles[theme]}`}
+      {...buttonProps}
+    >
       {children}
     </button>
   );
