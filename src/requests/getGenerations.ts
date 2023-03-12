6321dda224a358bc6e76d929bfd9d3ada1ba96c3
Luan <luan.sergio@novahaus.com.br>
2023-03-12 05:15:08 -0300

    feat: add get pokemon generations request function

---
+++ src/requests/getGenerations.ts
@@ -0,0 +1,25 @@
+import graphqlClient from '@services/api';
+
+async function fetchGenerations() {
+  const query = `
+  query MyQuery {
+    pokemon_v2_generation {
+      id
+      name
+    }
+  }
+  
+  `;
+
+  const result = await graphqlClient.request(query);
+
+  return result;
+}
+
+export default async function getGenerations(): Promise<IGeneration[]> {
+  const response = await fetchGenerations();
+
+  const generations = response.pokemon_v2_generation;
+
+  return generations;
+}
