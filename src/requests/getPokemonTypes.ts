551f8ae695b20c8f786d320ef49b1c4c77f54afa
Luan <luan.sergio@novahaus.com.br>
2023-03-12 05:14:50 -0300

    feat: add get pokemon types request function

---
+++ src/requests/getPokemonTypes.ts
@@ -0,0 +1,25 @@
+import graphqlClient from '@services/api';
+
+async function fetchPokemonTypes() {
+  const query = `
+  query MyQuery {
+    pokemon_v2_type {
+      id
+      name
+    }
+  }
+  
+  `;
+
+  const result = await graphqlClient.request(query);
+
+  return result;
+}
+
+export default async function getPokemonTypes(): Promise<IPokemonType[]> {
+  const response = await fetchPokemonTypes();
+
+  const pokemonTypes = response.pokemon_v2_type;
+
+  return pokemonTypes;
+}
