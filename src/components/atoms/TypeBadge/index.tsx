c543091642823d5b5538e21eefc8f6809a32d6db
Luan <luan.sergio@novahaus.com.br>
2023-03-15 17:53:47 -0300

    feat: add TypeBadge component

---
+++ src/components/atoms/TypeBadge/index.tsx
@@ -0,0 +1,25 @@
+import { ReactNode } from 'react';
+import styles from './styles.module.scss';
+
+interface ITypeBadge {
+  children: string;
+  size?: 'small' | 'medium';
+  modifier?: string;
+}
+
+const TypeBadge = ({
+  children,
+  modifier,
+  size = 'medium',
+}: ITypeBadge): JSX.Element => (
+  <div
+    className={`${styles.typeBadge} ${styles[size]} ${styles[children]} ${
+      modifier && `${styles.typeBadgeWithModifier}`
+    }`}
+  >
+    {children}
+    {modifier && <span className={styles.modifier}>2x</span>}
+  </div>
+);
+
+export default TypeBadge;
