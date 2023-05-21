87fcc731ca39ace5cb8739d02d5e4b83257cfade
Luan <luan.sergio@novahaus.com.br>
2023-05-21 05:50:48 -0300

    feat: add use move list hook

---
+++ src/presentation/hooks/useMovesList.ts
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react';
+import useSWR from 'swr';
+
+import createGetMovesByPokemonIdUsecase from '@factories/createGetMovesByPokemonIdUsecase';
+import Move from '@domain/entities/Move';
+
+interface UseMoveListParams {
+  pokemonId: number;
+}
+
+interface UseMoveListResponse {
+  moveList: Move[] | undefined;
+  isLoading: boolean;
+}
+
+export default function useMoveList({
+  pokemonId,
+}: UseMoveListParams): UseMoveListResponse {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const getMovesByPokemonIdUsecase = createGetMovesByPokemonIdUsecase();
+
+  const { data, error } = useSWR(`${pokemonId}`, async () => {
+    const response = await getMovesByPokemonIdUsecase.getByPokemonId(pokemonId);
+
+    if (response.isRight()) {
+      return response.value;
+    }
+
+    throw response.value;
+  });
+
+  useEffect(() => {
+    if (error != null || data != null) {
+      setIsLoading(false);
+    }
+  }, [data, error]);
+
+  return {
+    moveList: data,
+    isLoading,
+  };
+}
