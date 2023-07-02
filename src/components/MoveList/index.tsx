514029e1023b654feccdc72dd0f5171e13ca7c3c
Luan <luan.sergio@novahaus.com.br>
2023-07-01 22:09:49 -0300

    feat: add not found state to move list

--- src/components/MoveList/index.tsx
+++ src/components/MoveList/index.tsx
@@ -52,107 +52,136 @@ const MoveList = ({ pokemonId }: MoveListProps): JSX.Element => {
       <div className={styles.moveListHolder}>
         <div className={styles.moveList}>
           {isLoading || moveList === undefined ? (
-            <div className={styles.loading}>
-              <div className={styles.loadingPokeball}>
-                <PokeBallIcon />
-              </div>
-              <span className={styles.loadingText}>
-                Loading
-                <span className={styles.loadingDots}>
-                  <LoadingDots />
+            <div className={styles.moveListBox}>
+              <div className={styles.moveListBoxHeader} />
+              <div className={styles.moveListBoxContainer}>
+                <div className={styles.loadingPokeball}>
+                  <PokeBallIcon />
+                </div>
+                <span className={styles.moveListMessage}>
+                  Loading
+                  <span className={styles.loadingDots}>
+                    <LoadingDots />
+                  </span>
                 </span>
-              </span>
+              </div>
             </div>
           ) : (
-            <table
-              className={`${styles.moveListTable} h-neutral-scroll`}
-              cellPadding={0}
-              cellSpacing={0}
-            >
-              <thead>
-                <tr className={styles.moveListTableHeaderRow}>
-                  <th className={`${styles.moveListHead} ${styles.nameHead}`}>
-                    Name
-                  </th>
-                  <th className={`${styles.moveListHead} ${styles.typeHead}`}>
-                    Type
-                  </th>
-                  <th
-                    className={`${styles.moveListHead} ${styles.categoryHead}`}
+            <>
+              {moveList[0].length > 0 ? (
+                <table
+                  className={`${styles.moveListTable} h-neutral-scroll`}
+                  cellPadding={0}
+                  cellSpacing={0}
+                >
+                  <thead>
+                    <tr className={styles.moveListTableHeaderRow}>
+                      <th
+                        className={`${styles.moveListHead} ${styles.nameHead}`}
+                      >
+                        Name
+                      </th>
+                      <th
+                        className={`${styles.moveListHead} ${styles.typeHead}`}
+                      >
+                        Type
+                      </th>
+                      <th
+                        className={`${styles.moveListHead} ${styles.categoryHead}`}
+                      >
+                        Category
+                      </th>
+                      <th
+                        className={`${styles.moveListHead} ${styles.accuracyHead}`}
+                      >
+                        Accuracy
+                      </th>
+                      <th
+                        className={`${styles.moveListHead} ${styles.powerHead}`}
+                      >
+                        Power
+                      </th>
+                      <th className={`${styles.moveListHead} ${styles.ppHead}`}>
+                        PP
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody
+                    className={`${styles.moveListMovesContainer} h-neutral-scroll`}
                   >
-                    Category
-                  </th>
-                  <th
-                    className={`${styles.moveListHead} ${styles.accuracyHead}`}
-                  >
-                    Accuracy
-                  </th>
-                  <th className={`${styles.moveListHead} ${styles.powerHead}`}>
-                    Power
-                  </th>
-                  <th className={`${styles.moveListHead} ${styles.ppHead}`}>
-                    PP
-                  </th>
-                </tr>
-              </thead>
-              <tbody
-                className={`${styles.moveListMovesContainer} h-neutral-scroll`}
-              >
-                {moveList?.map((list, index) => (
-                  <Fragment key={index}>
-                    {list.map(move => (
-                      <tr className={styles.moveListMovesRow} key={move.name}>
-                        <td
-                          className={`${styles.moveListCell} ${styles.nameCell}`}
-                        >
-                          {formatPokemonMoveProperty(move.name)}
-                        </td>
-                        <td
-                          className={`${styles.moveListCell} ${styles.typeCell}`}
-                        >
-                          <div className={styles.typeContainer}>
-                            <TypeBadge size="small">{move.type}</TypeBadge>
+                    {moveList?.map((list, index) => (
+                      <Fragment key={index}>
+                        {list.map(move => (
+                          <tr
+                            className={styles.moveListMovesRow}
+                            key={move.name}
+                          >
+                            <td
+                              className={`${styles.moveListCell} ${styles.nameCell}`}
+                            >
+                              {formatPokemonMoveProperty(move.name)}
+                            </td>
+                            <td
+                              className={`${styles.moveListCell} ${styles.typeCell}`}
+                            >
+                              <div className={styles.typeContainer}>
+                                <TypeBadge size="small">{move.type}</TypeBadge>
+                              </div>
+                            </td>
+                            <td
+                              className={`${styles.moveListCell} ${styles.categoryCell}`}
+                            >
+                              <div className={styles.iconContainer}>
+                                {moveCategoryIcon[move.category]}
+                              </div>
+                            </td>
+                            <td
+                              className={`${styles.moveListCell} ${styles.accuracyCell}`}
+                            >
+                              {formatPokemonMoveProperty(move.accuracy)}
+                            </td>
+                            <td
+                              className={`${styles.moveListCell} ${styles.powerCell}`}
+                            >
+                              {formatPokemonMoveProperty(move.power)}
+                            </td>
+                            <td
+                              className={`${styles.moveListCell} ${styles.ppCell}`}
+                            >
+                              {formatPokemonMoveProperty(move.pp)}
+                            </td>
+                          </tr>
+                        ))}
+                      </Fragment>
+                    ))}
+
+                    {moveList?.[moveList?.length - 1].length >=
+                      MOVE_PER_REQUEST && (
+                      <tr className={styles.moveListMovesRowLoading}>
+                        <td className={styles.loaderCell}>
+                          <div id="loader">
+                            {[...Array(3)].map((x, i) => (
+                              <MoveListItemSkeleton key={i} />
+                            ))}
                           </div>
-                        </td>
-                        <td
-                          className={`${styles.moveListCell} ${styles.categoryCell}`}
-                        >
-                          <div className={styles.iconContainer}>
-                            {moveCategoryIcon[move.category]}
-                          </div>
-                        </td>
-                        <td
-                          className={`${styles.moveListCell} ${styles.accuracyCell}`}
-                        >
-                          {formatPokemonMoveProperty(move.accuracy)}
-                        </td>
-                        <td
-                          className={`${styles.moveListCell} ${styles.powerCell}`}
-                        >
-                          {formatPokemonMoveProperty(move.power)}
-                        </td>
-                        <td
-                          className={`${styles.moveListCell} ${styles.ppCell}`}
-                        >
-                          {formatPokemonMoveProperty(move.pp)}
                         </td>
                       </tr>
-                    ))}
-                  </Fragment>
-                ))}
-
-                {moveList?.[moveList?.length - 1].length >=
-                  MOVE_PER_REQUEST && (
-                  <tr className={styles.moveListMovesRow}>
-                    <td className={styles.loaderCell}>
-                      <div id="loader">
-                        <MoveListItemSkeleton />
-                      </div>
-                    </td>
-                  </tr>
-                )}
-              </tbody>
-            </table>
+                    )}
+                  </tbody>
+                </table>
+              ) : (
+                <>
+                  <div className={styles.moveListBox}>
+                    <div className={styles.moveListBoxHeader} />
+                    <div className={styles.moveListBoxContainer}>
+                      <span className={styles.moveListMessage}>
+                        There is no move information for this pokemon form yet.
+                      </span>
+                    </div>
+                  </div>
+                </>
+              )}
+            </>
           )}
         </div>
         <div className={styles.pokeballDecoration}>
